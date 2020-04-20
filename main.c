c75161401c9e326b4aeb83138b3e961cc38cb6cd
Rich-MJacques <63755047+Rich-MJacques@users.noreply.github.com>
2020-04-20 14:45:33 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,42 +1,28 @@
 #include "list.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 int main(){
-    printf("Creare la lista:");
-    create_list();
+    list *l= 
+    l=create_list();
+    add_init(l,3);
+    delete_list(l);
+    add_init(l,4);
+     print_list(l);
+    add(l,5,2);
+    add(l,7,3);
+     print_list(l);
+    remove_data(l,4);
+     print_list(l);
+    search_pos(l,7);
+    search_data(l,5);
+    print_list(l);
+    delete_end(l);
+    delete_init(l);
+     print_list(l);
+    printf("Terminé");
+    return 0;
+
 }
 
 
-void switch_para_agregar(){
-    printf("Que desea hacer?");
-    printf("1. Agregar al inicio \n 2. Agregar al final \n Agregar en una pocision");
-    scanf("%d",int e);
-    switch(e){
-        case 1: 
-            printf("Que dato agregará?");
-            scanf("%d",int e2);
-            add_init(t,12);
-            printf("imprimiendo lista");
-            printf("%list",t);
-            break;
-        case 2:
-            printf("Que dato agregará?");
-            scanf("%d",int e3);
-            add_end(t,14);
-            printf("imprimiendo lista");
-            printf("%list",t);
-            break;
-        case 3:
-            printf("Que dato agregará?");
-            scanf("%d",int e4);
-            add(t,13,1);
-            printf("imprimiendo lista");
-            printf("%list",t);
-            break;
-            
-
-    }
-    printf("Desea hacer otra accion? escriba 10");
-    scanf("%d",int e5);
-    if(e5==10) switch_para_agregar();
-}
